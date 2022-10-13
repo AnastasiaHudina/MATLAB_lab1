620f2956773436e88c503546136c0361e521b280
AnastasiaHudina <hudina.aa@edu.spbstu.ru>
2022-10-13 19:03:58 +0300

    13/10/22

--- lab2/lab2_ASP_HudinaA.m
+++ lab2/lab2_ASP_HudinaA.m
@@ -1,14 +1,18 @@
 %% Read and listen
 [bass, Fs] = audioread("bass.mp3");
-%sound(bass, Fs);
+sound(bass, Fs);
 [drums, ~] = audioread("drums.mp3");
+sound(drums, Fs);
 [synths, ~] = audioread("synths.mp3");
+sound(synths, Fs);
 [guitars, ~] = audioread("guitars.mp3");
-
+sound(guitars, Fs);
+%опред длительность
 szBass=size(bass);
 Ts=1/Fs;
 dur=Ts*szBass(1);
 
+%another variant?
 %bassdur = length(bass)/Fs;
 %drumsdur = length(drums)/Fs;
 %synthsdur = length(synths)/Fs;
@@ -27,11 +31,30 @@ t=(0:1:length(bass)-1)*Ts;
 f=2/(length(bass));
 volumeMod=A*sin(2*pi*f*timeArray);
 
+%teacher said
 %f = 2 / (lengthBass);
-%t = (0:length(bass) - 1) / fs;
+%t = (0:length(bass) - 1) / Fs;
 %volumeMod = sin(2*pi*f*t);
 
 volumeMod=volumeMod';
 sound(melody.*volumeMod, Fs);
 sound(melody.*(volumeMod.*volumeMod*2-1), Fs);
 %% Shorten melody
+%point a
+X = MelodyMatrix';
+tmp = X(1:1:length(X(:))/2);
+halfMelodyMatrix = reshape(tmp, 4,length(tmp)/4);
+
+%фффффффффффуtmp1 = reshape(halfMelodyMatrix, length(tmp)/4,4);
+
+halfMelody = sum(halfMelodyMatrix);
+sound(halfMelody,Fs)
+%point b
+halfMelody1 = melody(1:end/2, 1);
+%% 2-channel sound
+melody1 = melody.*(melody>0.5);
+melody2 = melody.*(melody<0.5);
+twochannel1 = [melody1, melody2];
+sound(twochannel1,Fs);
+twochannel2 = [melody flip(melody)];
+sound(twochannel2,Fs);
