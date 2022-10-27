d07b285787d21a3efcb5792e0b85c146b439e516
AnastasiaHudina <hudina.aa@edu.spbstu.ru>
2022-10-27 18:57:33 +0300

    cowabunga

--- lab3/filteringBanks.m
+++ lab3/filteringBanks.m
@@ -1,8 +1,24 @@
-function signalOut = filteringBanks(signal, f_bank, gain, typeOfFilter)
-f_bank_new = sum(gain .* f_bank);
+function [signalOut, initB] = filteringBanks(signal, f_bank, gain, typeOfFilter, initB)
+f_bank_new = sum(gain .* f_bank,1);
+
 switch typeOfFilter
     case 'filter'
-        signalOut = filter(f_bank_new, 1, signal);
+        [signalOut, initB] = filter(f_bank_new, 1, signal, initB);
     case 'fftfilt'
         signalOut = fftfilt(f_bank_new, signal);
+    case 'convFilter'
+        signalOut = convFilter(f_bank_new, signal);    
+end
+end
+function signalOut2 = convFilter(bBank, signal)
+    sizeA = size(signal);
+    if sizeA(1) < sizeA(2)
+        for i = 1:sizeA(1)
+            signalOut2(i,:) = conv(signal(i,:), bBank);
+        end
+    else
+        for i = 1:sizeA(2)
+            signalOut2(:,i) = conv(signal(:,i), bBank);
+        end
+    end
 end
--- lab3/lab3_ASP_HudinaA.m
+++ lab3/lab3_ASP_HudinaA.m
@@ -1,10 +1,25 @@
 %% Filters design
-freqArray1 = [31, 62, 125, 250, 500, 1000, 2000, 4000, 8000,16000];
+freqArray1 = [31, 62, 125, 250, 500, 1000, 2000, 4000, 8000, 16000];
 order1 = 1024; % должен быть четным
+%% 
 fS = 44100;
 result = CreateFilters(freqArray1, order1, fS);
-%% Filtering of signals
-[mp3,fS] = audioread('song.mp3');
+
+%% filtering of signals
+[mp3, fS] = audioread('Stronger.mp3');
 gain = ones(length(freqArray1), 1);
-tic; signalOut0 = filteringBanks(mp3, result, gain, 'filter'); toc;
-tic; signalOut1 = filteringBanks(mp3, result, gain, 'ffilter'); toc;
+initB = zeros(1, order1);
+tic; [signlout0, initB] = filteringBanks(mp3, result, gain, 'filter', initB); toc;
+tic; signlout1 = filteringBanks(mp3, result, gain, 'fftfilt', initB); toc;
+tic; signlout2 = filteringBanks(mp3, result, gain, 'convFilter', initB); toc; 
+%% Stream sound
+deviceWriter = audioDeviceWriter('SampleRate', fS);
+fileReader = dsp.AudioFileReader('Stronger.mp3');
+gain = [10 10 10 0.1*ones(1, 7)]';
+gain=ones(10,1);
+while ~isDone(fileReader)
+    gain = rand(size(freqArray1))'; 
+    audiodata = fileReader();
+    [dataPortionFiltering,initB]=filteringBanks(audiodata,result,gain,'filter',initB); 
+    deviceWriter(dataPortionFiltering);
+end
